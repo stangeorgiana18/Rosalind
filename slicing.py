4a27b77395e128850fd6634b4ea6928ebc85a88c
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-02-07 13:54:34 +0000

    improved methods

--- slicing.py
+++ slicing.py
@@ -1,5 +1,20 @@
-def slice(text, a, b, c, d):
-    return text[a:b+1] + ' ' + text[c:d+1]
+def slice_string(file_name):
+    with open(file_name, 'r') as file:
+        content = file.read()
+        file_content = content.split('\n') # numbers and text are on diff lines
+        text = file_content[0]
+        index = list(map(lambda x: int(x), file_content[1].split())) # apply lambda to each el in the list from file_content..
+        # map turned into a list to access the values by index 
+        
+    return text[index[0] : index[1] +1], text[index[2]: index[3] + 1]
 
-text = 'Idr53M4hDPnrLDmgijbp8TLik5GOq5HN5iLCuNe0tmwVabVYj7pslorZEpZEMacrorhamphusVNeJMtWqjQvnx9j1mZPSKjzqPDcLS3xhlEM66tKizj32RKlOgscRl2ADFOjr47bmWQ5wq3FYVbonasusR4ltkUg9f4qoYgVo1i6Pa2mwB.'
-print(slice(text, 60, 72, 146, 152))
+print(slice_string('rosalind_ini3.txt')) # result as tuple
+
+
+# ANOTHER WAY
+
+with open('rosalind_ini3.txt') as f:
+    line = f.readline() # read the first line of the file 
+    a, b, c, d = [int(x) for x in f.readline().split()] # read the 2nd line of the file and split it into a list of strings
+
+    print(line[a : b+1],line[c : d+1])
