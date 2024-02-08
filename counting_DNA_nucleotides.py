8b80ff5a625bd2783db25b309a8f68425ed54b0d
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-02-08 00:06:54 +0000

    use unpacking operator

--- counting_DNA_nucleotides.py
+++ counting_DNA_nucleotides.py
@@ -1,7 +1,11 @@
 def count_nucl(file_name):
     with open(file_name, 'r') as f:
         dna = f.read().strip()
-        for i in 'ACGT':
-            print(dna.count(i), end = " ")
+        
+        return map(dna.count, "ACGT")
 
-count_nucl('rosalind_dna.txt')
+result = count_nucl('rosalind_dna.txt')
+print(*result) # unpacking operator for the iterable elements
+
+# equivalent to: print(result[0], result[1], result[2], result[3])
+
