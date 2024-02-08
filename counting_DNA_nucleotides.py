e1287f4ff5b5b47020530f9feae0de0ca588ba92
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-02-08 07:40:32 +0000

    transcr

--- counting_DNA_nucleotides.py
+++ counting_DNA_nucleotides.py
@@ -9,3 +9,29 @@ print(*result) # unpacking operator for the iterable elements
 
 # equivalent to: print(result[0], result[1], result[2], result[3])
 
+
+def transcription(file_name):
+    with open(file_name, 'r') as f:
+        dna = f.read().strip()
+
+        rna = ''.join(['U' if base == 'T' else base for base in dna])
+
+        # or with map and lambda
+        # rna = ''.join(map(lambda base: 'U' if base == 'T' else base, dna))
+
+        # or using replace
+        #rna = dna.replace('T', 'U')
+
+        # alternatively
+        # rna = 'U'.join(dna.split('T'))
+
+        return rna
+    
+result = transcription('rosalind_rna.txt')
+print(result)
+
+
+# in bash:
+# cat rosalind_rna.txt | tr T U
+# tr - translate/transliterate to replace characters 
+
